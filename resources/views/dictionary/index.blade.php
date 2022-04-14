03113c59fb45875bcb4e39eb95f0d5bf85e6400a
radiomerz <radiomerz@gmail.com>
2022-04-14 18:00:50 +0700

    ADD publish button and relocate delete button in type content

--- resources/views/dictionary/index.blade.php
+++ resources/views/dictionary/index.blade.php
@@ -2,33 +2,59 @@
 @extends('admin.main')
 @section('content')
 
-    <div class="container">
-        <p></p>
-        <a class="btn btn-info" href="{{ route('home') }}"> Домой</a>
+    <div class="">
         <div class="d-flex justify-content-center"><h1>Справочники</h1></div>
+        <div class="row mt-4">
+            <div class="header-block row">
+                <div class="search-button col-2">
+                    <button id="hideshow" class="btn btn-primary"><span class="fa fa-search fa-lg" aria-hidden="true"></span></button>
+                </div>
+                <div class="search-form col-8">
+                    <div class="form">
+                        <form action="{{route('dictionary.store')}}" method="post">
+                            @csrf
+                            <div class="form-group row">
+                                <div class="col-4">
+                                    <input autocomplete="off" type="text" name="name" placeholder="Код справочника" id="code" class="form-control">
+                                </div>
+                                <div class="col-4">
+                                    <input autocomplete="off" type="text" name="name" placeholder="Наименование справочника" id="name" class="form-control">
+                                </div>
+                                <div class="col-4">
+                                    <select id="status" class="form-control" name="archive">
+                                        <option value='2'>Все</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="create col-2">
+                    <button id="clean" class="btn btn-primary"><span class="fa fa-paint-brush fa-lg" aria-hidden="true"></span> Очистить</button>
+                    <a href="{{route('type-content.create')}}" class="btn-create btn btn-primary"><span class="fa fa-plus-circle fa-lg" aria-hidden="true"></span></a>
+                </div>
+            </div>
+        </div>
         <table class="table table-bordered table-hover">
             <tr>
-                <th>ID</th>
-                <th>Код</th>
-                <th>Имя</th>
-                <th>Описание</th>
-                <th>Архив</th>
-                <th>Создатель</th>
-                <th>Дата создания</th>
-                <th>Последний редактор</th>
-                <th>Дата редактирования</th>
+                <th>Код справочника</th>
+                <th>Наименование справочника</th>
+                <th>Описание справочника</th>
+                <th>Последнее изменение</th>
+                <th>Статус</th>
             </tr>
             @foreach($dictionaries as $dictionary)
                 <tr>
-                    <td>{{$dictionary->id}}</td>
                     <td>{{$dictionary->code}}</td>
                     <td>{{$dictionary->name}}</td>
                     <td>{{$dictionary->description}}</td>
-                    <td>{{$dictionary->archive?'Да':'Нет'}}</td>
-                    <td {{$dictionary->created_author == Auth::guard('web')->user()->id?'class="bg-danger"':''}}>{{\App\Models\User::where('id', $dictionary->created_author)->first()->name}}</td>
-                    <td>{{$dictionary->created_at}}</td>
-                    <td>{{\App\Models\User::where('id', $dictionary->updated_author)->first()->name}}</td>
-                    <td>{{$dictionary->updated_at}}</td>
+                    <?php $up_at = date_create($dictionary->updated_at); ?>
+                    <td>{{date_format($up_at, 'd.m.Y H:m')}}</td>
+                    @if($dictionary->archive)
+                        <td class="text-danger">Архивный</td>
+                    @else
+                        <td class="text-success">Действующий</td>
+                   @endif
                     <td nowrap>
                         <a href="{{route('dictionary.show', ($dictionary->id))}}"
                            class="btn btn-success ">
