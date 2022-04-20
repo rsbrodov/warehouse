9a73f08a33c3ebafa8e26adedbe5d646116bf5d1
radiomerz <radiomerz@gmail.com>
2022-04-20 19:22:06 +0700

    TRANSFER dictionary to VueJS

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@ import Moment from 'vue-moment';
 import BootstrapVue from 'bootstrap-vue';
 
 Vue.component('Index', require('./components/type-content/Index.vue').default);
+Vue.component('DictionaryIndex', require('./components/dictionary/Index.vue').default);
 Vue.use(Vuelidate)
 Vue.use( Moment );
 Vue.use(BootstrapVue);
