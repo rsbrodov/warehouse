8114f8bc41cdee855d1fea22c32c41405cf091bc
radiomerz <radiomerz@gmail.com>
2022-05-02 21:17:07 +0700

    CONNECT typecontent with dictionary

--- resources/views/type_content/enter.blade.php
+++ resources/views/type_content/enter.blade.php
@@ -8,33 +8,36 @@
                     @foreach ($column['element'] as $field)
                         <div class="block mt-4">
                             <label for="{{$field['name']}}" class="">{{$field['title']}}</label>
-
                             @if($field['type'] === 'text')
-                                <input autocomplete="off" type="{{$field['type']}}"
-                                       class="form-control @error($field['name']) is-invalid @enderror"
-                                       id="{{$field['name']}}" name="{{$field['name']}}">
-                            @elseif($field['type'] === 'checkbox')
-                                @foreach($field['parameters'] as $params)
+                                <input autocomplete="off" type="{{$field['type']}}" class="form-control @error($field['name']) is-invalid @enderror" id="{{$field['name']}}" name="{{$field['name']}}">
+                            @elseif($field['type'] === 'checkbox' or $field['type'] === 'radio')
+{{--                                @foreach($field['parameters'] as $params)--}}
+{{--                                    <div>--}}
+{{--                                        <input type="{{$field['type']}}" id="{{$field['name']}}" name="{{$field['name']}}" value="{{$params['value']}}">--}}
+{{--                                        <label for="{{$params['value']}}">{{$params['label']}}</label>--}}
+{{--                                    </div>--}}
+{{--                                @endforeach--}}
+                                @foreach($dictionary_elems = \App\Models\DictionaryElement::where('dictionary_id', $field['parameters'])->get() as $dictionary_elem)
                                     <div>
-                                        <input type="checkbox" id="{{$params['name']}}" name="{{$params['name']}}"
-                                               value="{{$params['label']}}">
-                                        <label for="{{$params['name']}}">{{$params['label']}}</label>
+                                        <input type="{{$field['type']}}" id="{{$field['name']}}" name="{{$field['name']}}" value="{{$dictionary_elem['id']}}">
+                                        <label for="{{$dictionary_elem['value']}}">{{$dictionary_elem['value']}}</label>
                                     </div>
                                 @endforeach
                             @elseif($field['type'] === 'textarea')
-                                <textarea name="{{$field['name']}}" id="{{$field['name']}}"
-                                          class="form-control"></textarea>
+                                <textarea name="{{$field['name']}}" id="{{$field['name']}}" class="form-control"></textarea>
                             @elseif($field['type'] === 'select')
-                                <select id="role" type="text" class="form-control" name="role">
-                                    @foreach($field['parameters'] as $params)
-                                        <?php //print_r($params); ?>
-                                        <option value="{{$params['name']}}">{{$params['label']}}</option>
+{{--                                <select id="{{$field['name']}}" type="text" class="form-control" name="{{$field['name']}}">--}}
+{{--                                    @foreach($field['parameters'] as $params)--}}
+{{--                                        <option value="{{$params['value']}}">{{$params['label']}}</option>--}}
+{{--                                    @endforeach--}}
+{{--                                </select>--}}
+                                <select id="{{$field['name']}}" class="form-control" name="{{$field['name']}}">
+                                    @foreach($dictionary_elems = \App\Models\DictionaryElement::where('dictionary_id', $field['parameters'])->get() as $dictionary_elem)
+                                        <option value="{{$dictionary_elem['id']}}">{{$dictionary_elem['value']}}</option>
                                     @endforeach
                                 </select>
-                                <?php //exit; ?>
                             @endif
-                            @error($field['name']) <span class="invalid-feedback"
-                                                         role="alert"><strong>{{ $message }}</strong></span>@enderror
+                            @error($field['name']) <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>@enderror
                         </div>
                     @endforeach
                 @endforeach
