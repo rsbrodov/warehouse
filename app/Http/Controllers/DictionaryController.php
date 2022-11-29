0ee1415b670411d2d834bc86e964161263a6a63a
radiomerz <radiomerz@gmail.com>
2022-11-29 16:43:33 +0700

    Services

--- app/Http/Controllers/DictionaryController.php
+++ app/Http/Controllers/DictionaryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Http\Requests\DictionaryRequest;
 use App\Models\Dictionary;
 use App\Models\DictionaryElement;
@@ -9,16 +10,19 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use App\Services\DictionaryService;
 
 class DictionaryController extends Controller
 {
+    public $dictionaryService;
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function __construct()
+    public function __construct(DictionaryService $service)
     {
+        $this->dictionaryService = $service;
         //$this->middleware('auth');
     }
 
@@ -29,116 +33,45 @@ class DictionaryController extends Controller
         }
     }
 
-    public function findDictionary()
-    {
-        if(Auth::guard('web')->check()) {
-            $dictionary = Dictionary::where(['created_author' => Auth::guard('web')->user()->id])->with('created_author:id,name')->with('updated_author:id,name')->orderBy('created_at', 'asc')->get();
-            return response()->json($dictionary);
-
-        }else if (Auth::guard('api')->check()) {
-            $dictionary = Dictionary::where(['created_author' => Auth::guard('api')->user()->id])->with('created_author:id,name')->with('updated_author:id,name')->orderBy('created_at', 'asc')->get();
-            return response()->json($dictionary);
-        }
-    }
-
-    public function findDictionaryID($id)
-    {
-        $dictionary = Dictionary::where(['id' => $id])->with('created_author:id,name')->with('updated_author:id,name')->orderBy('created_at', 'asc')->get();
-        if($dictionary){
-            return response()->json($dictionary);
-        }else{
-            return response()->json('not found');
-        }
-
-
-    }
-
     public function store(DictionaryRequest $request)
     {
-        if (Auth::guard('web')->check()) {
-            $newDictionary = Dictionary::create([
-                'code' => $request->code,
-                'name' => $request->name,
-                'description' => $request->description,
-                'archive' => 0,
-                'created_author' => Auth::guard('web')->user()->id,
-                'updated_author' => Auth::guard('web')->user()->id
-            ]);
-            $dictionary = Dictionary::where('id', $newDictionary->id)->with('created_author:id,name')->with('updated_author:id,name')->get();
-            return response()->json($dictionary);
-
-        } else if (Auth::guard('api')->check()) {
-            $dic = Dictionary::create(['code' => $request->code, 'name' => $request->name, 'description' => $request->description, 'archive' => 0, 'created_author' => Auth::guard('api')->user()->id, 'updated_author' => Auth::guard('api')->user()->id]);
-            $dictionary = Dictionary::where('id', $dic->id)->with('created_author:id,name')->with('updated_author:id,name')->get();
-            return response()->json($dictionary);
-        }
+        $result = $this->dictionaryService->store($request);
+        return $result;
     }
-
 
     public function show($id)
     {
-
-        if (Auth::guard('web')->check()) {
-            $user = Auth::guard('web')->user();
-            if ($user->hasRole('Admin') or $user->hasRole('SuperAdmin')) {
-                $dictionaryElements = DictionaryElement::where('dictionary_id', $id)->get();
-                return view('dictionary.show', ['dictionary_elements' => $dictionaryElements, 'dictionary_id' => $id]);
-            }
-        } else if (Auth::guard('api')->check()) {
-            $dictionaryElement = DictionaryElement::where(['dictionary_id' => $id])->with('created_author:id,name')->with('updated_author:id,name')->get();
-            return response()->json($dictionaryElement);
-        }
+        $result = $this->dictionaryService->show($id);
+        return $result;
     }
-
 
     public function update(DictionaryRequest $request, $id)
     {
-        if (Auth::guard('web')->check()) {
-            $dictionary = Dictionary::find($id);
-            $dictionary->name = $request->name;
-            $dictionary->description = $request->description;
-            $dictionary->code = $request->code;
-            $dictionary->updated_author = Auth::guard('web')->user()->id;
-            $dictionary->save();
-            $dictionary = Dictionary::find($id)->with('created_author:id,name')->with('updated_author:id,name')->get();
-            return response()->json($dictionary);
-
-        } else if (Auth::guard('api')->check()) {
-            $dictionary = Dictionary::find($id);
-            $dictionary->name = $request->name;
-            $dictionary->description = $request->description;
-            $dictionary->code = $request->code;
-            $dictionary->updated_author = Auth::guard('api')->user()->id;
-            $dictionary->save();
-            $dictionary = Dictionary::find($id)->with('created_author:id,name')->with('updated_author:id,name')->get();
-            return response()->json($dictionary);
-        }
-    }
-
-
-    public function archive($id)
-    {
-        if (Auth::guard('web')->check()) {
-            $dictionary = Dictionary::where('id', $id)->first();
-            if ($dictionary->archive == 1) {
-                $dictionary->archive = 0;
-            } else {
-                $dictionary->archive = 1;
-            }
-            $dictionary->save();
-            return response()->json($dictionary);
-        }
+        $result = $this->dictionaryService->update($request, $id);
+        return $result;
     }
 
     public function destroy($id)
     {
+        $result = $this->dictionaryService->destroy($id);
+        return $result;
+    }
 
-        $dictionary = Dictionary::find($id);
-        if ($dictionary) {
-            $dictionary->delete();
-            return response()->json('item was deleted');
-        }else {
-            return response()->json('item not found');
-        }
+    public function archive($id)
+    {
+        $result = $this->dictionaryService->archive($id);
+        return $result;
+    }
+
+    public function findDictionary()
+    {
+        $result = $this->dictionaryService->findDictionary();
+        return $result;
+    }
+
+    public function findDictionaryID($id)
+    {
+        $result = $this->dictionaryService->findDictionaryID($id);
+        return $result;
     }
 }
--- app/Http/Controllers/ElementContentController.php
+++ app/Http/Controllers/ElementContentController.php
@@ -8,26 +8,16 @@ use App\Models\TypeContent;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
+use App\Services\ElementContentService;
+
 class ElementContentController extends Controller
 {
-    public function __construct()
+    public $elementContentService;
+
+    public function __construct(ElementContentService $service)
     {
+        $this->elementContentService = $service;
         //$this->middleware('auth');
-    }
-    public function findElementContentID($id)
-    {
-        if (Auth::guard('web')->check()) {
-            $typeContent = TypeContent::find($id);
-            $typeContents = TypeContent::where('id_global', $typeContent->id_global)->pluck('id');
-            $elementContents = ElementContent::
-                whereIn('type_content_id', $typeContents)
-                ->with('created_authors:id,name')
-                ->with('updated_authors:id,name')
-                ->orderBy('created_at', 'asc')
-                ->get()
-                ->unique('id_global');//все уникальные
-            return $elementContents;
-        }
     }
 
     public function index()
@@ -37,203 +27,55 @@ class ElementContentController extends Controller
 
     public function store(ElementContentRequest $request)
     {
-        //return $request;
-        if (Auth::guard('web')->check()) {
-            $newElementContent = ElementContent::create(
-                [
-                    'id_global'      => Str::uuid()->toString(),
-                    'type_content_id'=> request('type_content_id'),
-                    'label'          => $request->label,
-                    'description'          => $request->description,
-                    'active_from'    => date_create($request->active_from),
-                    'active_after'   => date_create($request->active_after),
-                    'status'         => 'DRAFT',
-                    'version_major'  => '1',
-                    'version_minor'  => '0',
-                    'api_url'            => str_slug($request->api_url),
-                    'based_element'  => null,
-                    'body'  => '',
-                    'created_author' => Auth::guard('web')->user()->id,
-                    'updated_author' => Auth::guard('web')->user()->id,
-                ]
-            );
-
-            return response()->json($newElementContent);
-        }
+        $result = $this->elementContentService->store($request);
+        return $result;
     }
+
     public function edit($id)
     {
-        if (Auth::guard('web')->check()) {
-            $user = Auth::guard('web')->user();
-            if ($user->hasRole('SuperAdmin') or $user->hasRole('Admin')) {
-                $elementContent = ElementContent::where('id', $id)->first();
-                return view('element_content.edit', ['element_content' => $elementContent]);
-            }
-        }
+        $result = $this->elementContentService->edit($id);
+        return $result;
     }
+
     public function update(Request $request)
     {
-        if (Auth::guard('web')->check()) {
-            $element = ElementContent::find($request->id);
-            if ($element->status === 'Draft' || $element->status === 'Archive') {
-                $otherPublished = ElementContent::where(['id_global' => $element->id_global, 'status' => 'Published'])->first();
-                if (isset($otherPublished)) {
-                    $otherPublished->status = 'Archive';
-                    $otherPublished->save();
-                }
-            }
-            //if (!$element->checkingApiUrl($request['url'], $element['id_global'])) {
-                $element->label = $request->label;
-                $element->api_url = $request->api_url;
-                $element->active_from = $request->active_from;
-                $element->active_after = $request->active_after;
-                $element->status = $request->status;
-                $element->description = $request->description;
-                $element->updated_author = Auth::guard('web')->user()->id;
-                $element->save();
-                $elementContent = ElementContent::find(request('id'))->with('created_authors:id,name')->with('updated_authors:id,name');
-
-           // } else {
-                return response()->json($elementContent);
-            //}
-        } /*else {
-            if (Auth::guard('api')->check()) {
-                $element = ElementContent::find(request('id'));
-                 $element->label = $request['label'];
-                 $element->owner = $request['owner'];
-                 $element->active_from = $request['active_from'];
-                 $element->active_after = $request['active_after'];
-                 $element->url = $request['url'];
-                 $element->body = $request['body'];
-                 $element->updated_author = Auth::guard('api')->user()->id;
-                 $element->save();
-                 $elementContent = ElementContent::find($element->id)->with('created_author:id,name')->with('updated_author:id,name')->get();
-                return response()->json($elementContent);
-            }*/
-        //}
+        $result = $this->elementContentService->update($request);
+        return $result;
     }
-    /*public function destroy(){
-        if (Auth::guard('web')->check()) {
-            $user = Auth::guard('web')->user();
-            if ($user->hasRole('SuperAdmin') or $user->hasRole('Admin')) {
-                $destroyElem = ElementContent::where('id', request('id'))->first();
-                $typeContentId = $destroyElem->type_content_id;
-                $message = ''; $typeMessage = 'success';
-                if ($destroyElem->status == 'Destroy') {
-                    $existOtherDraft = ElementContent::where(['id_global'=>$destroyElem->id_global, 'status' => 'Draft'])->count();
-                    if($existOtherDraft){
-                        return redirect()->back()->with('error', 'Вы не сможете восстановить этот элемент, пока не закончите работу с существующим черновиком');
-                    }
-                    $destroyElem->status = 'Draft';
-                    $message = 'Элемент ' . $destroyElem->label . ' восстановлен';
-                } else {
-                    $destroyElem->status = 'Destroy';
-                    $message = 'Элемент ' . $destroyElem->label . ' удален';
-                    $typeMessage = 'info';
-                }
-                $destroyElem->save();
-                return redirect()->route('element-content.index', $typeContentId)->with($typeMessage, $message);
-            }
-        }
-    }*/
 
     public function destroy($id)
     {
-        $typeContent = ElementContent::find($id);
-        if ($typeContent) {
-            $typeContent->delete();
-            return response()->json('item was deleted');
-        } else {
-            return response()->json('item not found');
-        }
+        $result = $this->elementContentService->destroy($id);
+        return $result;
+    }
+
+    public function findElementContentID($id)
+    {
+        $result = $this->elementContentService->findElementContentID($id);
+        return $result;
     }
 
     public function createNewVersion($id, $parameter)
     {
-        $element = ElementContent::find($id);
-        $existOtherDraft = ElementContent::where(['id_global'=>$element->id_global, 'status' => 'Draft'])->count();
-        if($existOtherDraft){
-            return redirect()->back()->with('error', 'Черновик уже существует. Удалите его или отредактируйте.');
-        }
-        //проверка параметров
-        if ($parameter == 'major' || $parameter == 'minor') {
-            if ($parameter == 'major') {
-                //если мажор то мы просто создаем дубликат наивысшей строки по version_major
-                $elementContent = ElementContent::where('id_global', $element->id_global)->orderBy('version_major', 'desc')->first();
-                //replicate - встроенный метод дублирования в laravel
-                $newElementContent = $elementContent->replicate();//тут лежит наш новый объект
-                $newElementContent->version_major = $elementContent->version_major + 1;//изменяем объект с учетом наших параметров затем сохраняем
-                $newElementContent->version_minor = 0;
-            } else {
-                //если минор то просто ищим наивысшей строки по version_major и version_minor ну и изменяем версию
-                $elementContent = ElementContent::where('id_global', $element->id_global)->orderBy('version_major', 'desc')->orderBy('version_minor', 'desc')->first();
-                $newElementContent = $elementContent->replicate();
-                $newElementContent->version_minor = $elementContent->version_minor + 1;
-            }
-            $newElementContent->status = 'Draft';
-            $newElementContent->based_element = $element->id;
-            if ($newElementContent->save()) {
-               return redirect()->route('element-content.get-all-version', $elementContent->id_global)->with('success', 'Новая версия успешно создана');
-                // return redirect()->route('element-content.index', $element->type_content_id)->with('success', 'Новая версия успешно создана');
-            } else {
-                return redirect()->back()->with('error', 'Что-то пошло не так');
-            }
-        } else {
-            return redirect()->back()->with('error', 'Что-то пошло не так');
-        }
+        $result = $this->elementContentService->createNewVersion($id, $parameter);
+        return $result;
     }
 
     public function getAllVersionElementContent()
     {
-        if (Auth::guard('web')->check()) {
-            $elementContents = ElementContent::where('id_global', request('id_global'))->orderBy('version_major', 'asc')->orderBy('version_minor', 'asc')->get();
-            return view('element_content.all-version-element-content')->with('element_contents', $elementContents);
-        } else {
-            if (Auth::guard('api')->check()) {
-                $elementContent = ElementContent::where('id_global', request('id_global'))->orderBy('version_major', 'asc')->orderBy('version_minor', 'asc')->get();
-                return response()->json($elementContent);
-            } else {
-                return response()->json('item not found');
-            }
-        }
+        $result = $this->elementContentService->getAllVersionElementContent();
+        return $result;
     }
 
     public function saveDraft(Request $request, $id)
     {
-        if (Auth::guard('web')->check()) {
-            $elementContent = ElementContent::find($id);
-            $typeContent = TypeContent::find($elementContent->type_content_id);
-            $body = json_decode($typeContent->body);
-            $error = [];
-            $i = 0;
-            foreach ($body as $row){
-                foreach ($row as $column){
-                    foreach ($column as $element){
-                        $i++;
-                        print_r($element->name.' : '.$request[$element->type.$i].'</br>');
-                        if ($element->required==1 and !$request[$element->type.$i]) {
-                            $error[] = [
-                                'errors' => [
-                                    'field_name'      =>  $element->name,
-                                    'field_type'      =>  $element->type.$i,
-                                    'filled_error' => 'Поле не заполнено',
-                                ]
-                            ];
-                        }
-                    }
-                }
-            }
-            dd($error);
-        }
+        $result = $this->elementContentService->saveDraft($request, $id);
+        return $result;
     }
 
     public function getApiUrl($apiUrl)
     {
-        $elementContent = ElementContent::where('api_url', $apiUrl)->first();
-        if (isset($elementContent->api_url)) {
-            print_r($elementContent);
-        } else {
-            print_r('Exception');
-        }
+        $result = $this->elementContentService->getApiUrl($apiUrl);
+        return $result;
     }
 }
